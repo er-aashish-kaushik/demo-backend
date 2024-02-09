e279d6a438f8b6eb0c6a8b71f9b81c519e568f6d
aashish-sharma-kellton <aashish.sharma@adani.com>
2024-02-09 18:06:46 +0530

    added cors

--- index.ts
+++ index.ts
@@ -2,10 +2,12 @@ import express from 'express';
 import dotenv from 'dotenv';
 import logger from './config/logger';
 import connectDB from './config/database/db'
+import cors from 'cors'
 import { userRouter } from './src/routes/userRoute'
 import mongoose from 'mongoose';
 dotenv.config()
 const app: express.Application = express();
+app.use(cors())
 app.use(express.json());
 app.use('/v1/user/', userRouter)
 const db = mongoose.connection;
